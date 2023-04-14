afdb103f8211de31ad409d8337c5786b3732570b
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-04-14 19:08:37 -0300

    function criptografar

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+As "chaves" de criptografia que utilizaremos são:
+A letra "e" é convertida para "enter"
+A letra "i" é convertida para "imes"
+A letra "a" é convertida para "ai"
+A letra "o" é convertida para "ober"
+A letra "u" é convertida para "ufat"
+*/
+function criptografar(texto){
+    
+   return texto;
+}
+
+var texto = "ao";
+document.write(criptografar(texto));
